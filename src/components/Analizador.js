986df6fb611ffc6dcdc118e581d41d1ee204f564
artherx <xxartherxx@gmail.com>
2024-04-17 22:16:37 -0500

    intento de anchura

--- src/components/Analizador.js
+++ src/components/Analizador.js
@@ -1,65 +1,62 @@
 //base
-
-// *encontrar la posicion vacia y darle su producto cuz
-function AnaliticVoid(emptyPi, tam) {
+  const actual = []
+function vecinoLeal(emptyPi) {
+  actual.push(emptyPi)
+  if (actual.length > 2) {
+      actual.shift();
+  }
+  return actual;
+}
+function AnaliticVoid(emptyPi, vecinoT,tam) {
   const col = emptyPi.col;
   const row = emptyPi.row;
   const validQuadrants = [];
-  // Verificar si la casilla está en el borde derecho
-  if (col < tam - 1) {
-    validQuadrants.push({ row: row, col: col + 1 }); // Casilla a la derecha
+  const vecino=[
+    {
+      row:vecinoT.row,
+      col:vecinoT.col
+    }
+  ]
+  console.log(emptyPi, vecino, tam)
+  // Verificar si la casilla está en la parte superior
+  if (row > 0 &&
+      vecino.row != row - 1 && vecino.col != col
+  ) {
+    console.log(`pa arriba`)
+      validQuadrants.push({ row: row - 1, col: col }); // Casilla arriba
+      return validQuadrants;
   }
 
   // Verificar si la casilla está en el borde izquierdo
-  if (col > 0) {
-    validQuadrants.push({ row: row, col: col - 1 }); // Casilla a la izquierda
+  if (col > 0 &&
+      vecino.row !== row  && vecino.col !== col-1
+  ) {
+    console.log(`pa izquierda`)
+      validQuadrants.push({ row: row, col: col - 1 }); // Casilla a la izquierda
+      return validQuadrants;
   }
 
   // Verificar si la casilla está en la parte inferior
-  if (row < tam - 1) {
-    validQuadrants.push({ row: row + 1, col: col }); // Casilla abajo
+  if (row < tam - 1&&
+      vecino.row!==row+1 && vecino.col!==col
+  ) {
+    
+    console.log(`pa bajo`)
+      validQuadrants.push({ row: row + 1, col: col }); // Casilla abajo
+      return validQuadrants;
   }
 
-  // Verificar si la casilla está en la parte superior
-  if (row > 0) {
-    validQuadrants.push({ row: row - 1, col: col }); // Casilla arriba
+  // Verificar si la casilla está en el borde derecho
+  if (col < tam - 1&&
+      vecino.row!==row && vecino.col!==col+1
+  ) {
+    console.log(`pa derecha`)
+      validQuadrants.push({ row: row, col: col + 1 }); // Casilla a la derecha
+      return validQuadrants;
   }
-
+  
+  console.log(`pa ningunlado`)
   return validQuadrants;
 }
 
-function calcularFichaAMover(piece, validQuadrants, piecesNew) {
-  const tablero = piece;
-  let fichaAMover = [];
-  let valorMinimo = Infinity; // Inicializamos el valor mínimo con infinito
-  let valorMinimoN = Infinity; // Inicializamos el valor mínimo con infinito
-
-  // Iteramos sobre cada cuadrante válido
-  for (const cuadrante of validQuadrants) {
-    var itera = 0;
-    const { row, col } = cuadrante;
-    const pieza = tablero.find((p) => p.row === row && p.col === col);
-    const real = piecesNew.find((p) => p.number == pieza.number);
-    console.log(`este es pieza row ${pieza.row} col ${pieza.col} number ${pieza.number}y este es el real row  ${real.row} col ${real.col} number ${real.number}`)
-    const d = Math.sqrt(
-      Math.pow(real.row-pieza.row,2)+Math.pow(real.col-pieza.col,2)
-    )
-    console.log(d)
-
-    // Verificamos si la ficha actual tiene un valor menor que el valor mínimo
-    if (pieza && pieza.number !== 0 &&d !== 0 && d < valorMinimo && pieza.number < valorMinimoN) {
-      itera++;
-      valorMinimoN= pieza.number;
-      valorMinimo = d;
-      fichaAMover[0] = row;
-      fichaAMover[1] = col;
-
-      console.log(`esta es la ficha ${fichaAMover}, su valor ${valorMinimo} y cauntas veces a pasado ${itera}`
-      );
-    }
-  }
-
-  return {fichaAMover};
-}
-
-export { AnaliticVoid, calcularFichaAMover };
+export { AnaliticVoid, vecinoLeal };
